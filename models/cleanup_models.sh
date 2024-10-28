994f17cc430b8ca52b8fdcfa5510debfb80eb7cc
Blumlaut <13604413+Blumlaut@users.noreply.github.com>
2024-10-28 14:29:10 +0100

    add all other cars

--- models/cleanup_models.sh
+++ models/cleanup_models.sh
@@ -5,8 +5,23 @@ extensions=("*.mat" "*.txt" "*Sponsors*.png")
 
 # Loop through each extension and remove matching files
 for ext in "${extensions[@]}"; do
-  find . -type f -name "$ext" -exec rm {} \;
+  find . -type f -name "$ext" -exec rm {} -v \;
 done
 
 # find and delete all files ending with .gltf and .bin unless they end with "_exterior.gtlf/bin"
-find . -type f \( -name "*.gltf" -o -name "*.bin" \) ! -name "*_exterior.gltf" ! -name "*_exterior.bin" -exec rm {} \;
+find . -type f \( -name "*.gltf" -o -name "*.bin" -o -name "*.md5mesh" \) ! -name "*_exterior.gltf" ! -name "*_exterior.bin" -exec rm {} -v \;
+
+rm -rfv */*Skeleton*/
+rm -rfv */display/
+rm -rfv */textures/
+rm -rfv */skins/20*/
+rm -rfv */skins/0_banner/
+rm -rfv */skins/preseason/
+rm -rfv */skins/custom/custom_fana
+rm -rfv */skins/custom/rim*
+rm -rfv */skins/custom/*/EXT_Skin_Colour.png
+rm -rfv */skins/custom/*/EXT_Skin_Mask.png
+
+
+# delete all folders 1 level deep that contain no files
+find . -type d -empty -delete
